878e6dd7f876ebe78cd94d64e6afc332df430e63
NicMuller <njmuller@outlook.com>
2016-09-24 08:03:32 +0200

    Add login and registration

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import re
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 mysql = MySQLConnector(app,'wall')
-app.secret_key = "13k34987"
+app.secret_key = "13k378987"
 
 def get_user(email):
     user_query = "SELECT * FROM users WHERE email = :email LIMIT 1"
@@ -18,12 +18,14 @@ def get_user(email):
 def index():
     return render_template('index.html')
 
-@app.route('/success', methods=['POST', 'GET'])
-def success():
-    return render_template('success.html')
+@app.route('/wall', methods=['GET'])
+def swall():
+    return render_template('wall.html')
 
 @app.route('/create_user', methods=['POST'])
 def create():
+    print "hit"
+    print request.form
 
     #validation  / Exist done in view with HTML
     first_name = request.form['first_name']
@@ -33,52 +35,42 @@ def create():
     password = request.form['password']
     confirm = request.form['password_confirm']
 
-    if len(first_name) < 2:
-        flash('Only letters and at least 2 characters in your name')
-        return redirect('/')
-    elif len(last_name) < 2:
-        flash('Only letters and at least 2 characters in your last name')
-        return redirect('/')
-    elif len(password) < 8:
-        flash('Password must be at least 8 characters')
-        return redirect('/')
-    elif not password == confirm:
-        flash('Password and Confirmation must match')
-        return redirect('/')
-    else:
-        #create user
+    pw_hash = bcrypt.generate_password_hash(password)
+    insert_query = "INSERT INTO users (first_name, last_name, email, pw_hash, created_at) VALUES (:first_name, :last_name, :email, :pw_hash, NOW())"
+    query_data = { 'first_name': first_name, 'last_name' : last_name, 'email': email,'pw_hash': pw_hash }
+    mysql.query_db(insert_query, query_data)
 
-        pw_hash = bcrypt.generate_password_hash(password)
-        insert_query = "INSERT INTO users (first_name, last_name, email, pw_hash, created_at) VALUES (:first_name, :last_name, :email, :pw_hash, NOW())"
-        query_data = { 'first_name': first_name, 'last_name' : last_name, 'email': email,'pw_hash': pw_hash }
-        mysql.query_db(insert_query, query_data)
+    #get/set user before success page
 
-        #get/set user before success page
+    user = get_user(email)
+    session['id'] = user[0]['id']
+    session['name'] = user[0]['first_name']
 
-        user = get_user(email)
-        session['id'] = user[0]['id']
-        session['name'] = user[0]['first_name']
-
-        return redirect('/success')
+    return redirect('/wall')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    email = request.form['email']
-    password = request.form['password']
-    user = get_user(email)
-    if not len(user) == 0 and bcrypt.check_password_hash(user[0]['pw_hash'], password):
-        session['id'] = user[0]['id']
-        session['name'] = user[0]['first_name']
-        return redirect('/success')
-    else:
-        flash("Please try again")
-        return redirect('/')
 
-@app.route('/logout', methods=['POST'])
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = get_user(email)
+        if len(user) != 0 and bcrypt.check_password_hash(user[0]['pw_hash'], password):
+            session['id'] = user[0]['id']
+            session['name'] = user[0]['first_name']
+            return redirect('/wall')
+        else:
+            flash("Please try again")
+            return redirect('/')
+    elif request.method == 'GET':
+        return render_template('login.html')
+
+@app.route('/logout', methods=['GET'])
 def logout():
-    if  session['id']:
-        session['id'] = ''
-        session['name'] = ''
+
+    if  'id' in session:
+        session.pop('id')
+        session.pop('name')
         print session
         return redirect('/')
     else:
